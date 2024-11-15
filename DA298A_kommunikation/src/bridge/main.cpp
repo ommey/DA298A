828dbdb7010166f07174ed23f032204470d9ab3a
ommey <omar.aayoubii@gmail.com>
2024-11-15 13:06:02 +0100

    okok

--- DA298A_kommunikation/src/bridge/main.cpp
+++ DA298A_kommunikation/src/bridge/main.cpp
@@ -1,15 +1,20 @@
 #include <Arduino.h>
+#include <painlessMesh.h>
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
+    Serial.begin(115200);
   int result = myFunction(2, 3);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Hello, world! from bridge");
+  delay(1000);
+
 }
 
 // put function definitions here:

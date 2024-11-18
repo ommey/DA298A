cec5a0c312e8b3024f59ed3660daa3efa6f2f84b
ommey <omar.aayoubii@gmail.com>
2024-11-19 00:49:19 +0100

    freertos implementerad

--- src/common/main.cpp
+++ src/common/main.cpp
@@ -5,28 +5,43 @@
 #define   MESH_PASSWORD   "meshPassword"
 #define   MESH_PORT       5555
 
-Scheduler userScheduler;
-namedMesh mesh;
+String bridgeNAme = "bridge"; // namnet på brygga-noden
+//
+String nodeName; // namnet på noden
+//
+namedMesh mesh; //variant på painlessMesh som kan skicka meddelanden till specifika noder
 
-String bridgeName = "bridge"; // Target node (bridge)
-String nodeName; // Unique name for this node
+void informBridge(){
+  String msg = "Hello from " + nodeName;
 
-//void broadCastCommon(String message);
-//void sendToBridge();
-
-Task taskSendMessage(TASK_SECOND * 10, TASK_FOREVER, []() {
-    String msg = "Hello from " + nodeName;
-    if (!mesh.sendSingle(bridgeName, msg)) {
+      if (!mesh.sendSingle(bridgeNAme, msg)) {
         Serial.println("Message send failed!");
+      }  
+}
+
+void informBridge(void *pvParameters) {
+    while (1)
+    {
+      informBridge();
+      vTaskDelay(1000 / portTICK_PERIOD_MS);    
     }
-});
+    
+    
+}
+
+void meshUpdate(void *pvParameters){
+    while(1) {
+        mesh.update();
+        vTaskDelay(50 / portTICK_PERIOD_MS);
+    }
+}
 
 void setup() {
   Serial.begin(115200);
   Serial.setTimeout(50);
 
   //mesh.setDebugMsgTypes(ERROR | CONNECTION); 
-  mesh.init(MESH_SSID, MESH_PASSWORD, &userScheduler, MESH_PORT);
+  mesh.init(MESH_SSID, MESH_PASSWORD, MESH_PORT);
 
   // Assign a unique name based on the node's ID
   nodeName = String(mesh.getNodeId());
@@ -41,11 +56,11 @@ void setup() {
   });
 
   // Add and enable the task for sending messages
-  userScheduler.addTask(taskSendMessage);
-  taskSendMessage.enable();
-  Serial.println(mesh.getName());
+  xTaskCreate(meshUpdate, "meshUpdate", 10000, NULL, 1, NULL);
+  xTaskCreate(informBridge, "informBridge", 10000, NULL, 1, NULL);
 }
 
-void loop() {
-  mesh.update();
+void loop() 
+{
+  // inget görs här, aktiviteter sköts i freeRTOS tasks
 }

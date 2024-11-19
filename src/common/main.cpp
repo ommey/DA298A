3f8dc1f9f0d76739fdb7d739e4bb847843726c26
ommey <omar.aayoubii@gmail.com>
2024-11-19 01:14:23 +0100

    gg

--- src/common/main.cpp
+++ src/common/main.cpp
@@ -11,18 +11,66 @@ String nodeName; // namnet på noden
 //
 namedMesh mesh; //variant på painlessMesh som kan skicka meddelanden till specifika noder
 
-void informBridge(){
-  String msg = "Hello from " + nodeName;
+void informBridge(void *pvParameters);
+void meshUpdate(void *pvParameters);
+void doFireFighterStuff(void *pvParameters);
+void fireFighterStuff();
 
-      if (!mesh.sendSingle(bridgeNAme, msg)) {
-        Serial.println("Message send failed!");
-      }  
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(50);
+
+  //mesh.setDebugMsgTypes(ERROR | CONNECTION); 
+  mesh.init(MESH_SSID, MESH_PASSWORD, MESH_PORT); // Starta meshen
+
+  nodeName = String(mesh.getNodeId()); //namnet kan modifieras mes.getNodeId() är alltid unikt
+  mesh.setName(nodeName); 
+
+  mesh.onReceive([](String &from, String &msg) {
+    Serial.printf("Received message from %s: %s\n", from.c_str(), msg.c_str());
+  });
+
+  mesh.onChangedConnections([]() {
+    Serial.printf("Connection table changed\n");
+  });
+
+  xTaskCreate(meshUpdate, "meshUpdate", 10000, NULL, 1, NULL);
+  xTaskCreate(informBridge, "informBridge", 10000, NULL, 1, NULL); 
+  xTaskCreate(doFireFighterStuff, "doFireFighterStuff", 10000, NULL, 1, NULL);
+}
+
+void loop() 
+{
+  // inget görs här, aktiviteter sköts i freeRTOS tasks
+}
+
+void fireFighterStuff(){
+  Serial.print("Branmannen inom mig jobbar hårt "); //här går tillståndsmaskinen in istället för denna printout
+}
+
+void doFireFighterStuff(void *pvParameters){
+  int work = 0;
+  while(1){
+    fireFighterStuff();
+    work++;
+    if (work > 1000)
+    {
+      work = 0;
+    }
+    Serial.println(work); //räknare som kan användas för att testa tick-funktionen
+
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
 }
 
 void informBridge(void *pvParameters) {
     while (1)
     {
-      informBridge();
+        String msg = "Hello from " + nodeName;
+
+      if (!mesh.sendSingle(bridgeNAme, msg)) {
+        Serial.println("Message send failed!");
+      }  
       vTaskDelay(1000 / portTICK_PERIOD_MS);    
     }
     
@@ -34,33 +82,4 @@ void meshUpdate(void *pvParameters){
         mesh.update();
         vTaskDelay(50 / portTICK_PERIOD_MS);
     }
-}
-
-void setup() {
-  Serial.begin(115200);
-  Serial.setTimeout(50);
-
-  //mesh.setDebugMsgTypes(ERROR | CONNECTION); 
-  mesh.init(MESH_SSID, MESH_PASSWORD, MESH_PORT);
-
-  // Assign a unique name based on the node's ID
-  nodeName = String(mesh.getNodeId());
-  mesh.setName(nodeName); 
-
-  mesh.onReceive([](String &from, String &msg) {
-    Serial.printf("Received message from %s: %s\n", from.c_str(), msg.c_str());
-  });
-
-  mesh.onChangedConnections([]() {
-    Serial.printf("Connection table changed\n");
-  });
-
-  // Add and enable the task for sending messages
-  xTaskCreate(meshUpdate, "meshUpdate", 10000, NULL, 1, NULL);
-  xTaskCreate(informBridge, "informBridge", 10000, NULL, 1, NULL);
-}
-
-void loop() 
-{
-  // inget görs här, aktiviteter sköts i freeRTOS tasks
 }

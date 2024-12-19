b5ae8d1eb7ffbc3eb09f73fdf92554f32c5b5827
ommey <omar.aayoubii@gmail.com>
2024-12-19 11:36:10 +0100

    comms klass klar

--- src/bridge/main.cpp
+++ src/bridge/main.cpp
@@ -8,113 +8,110 @@
 
 painlessMesh mesh; //variant på painlessMesh som kan skicka meddelanden till specifika noder
 
-class Comms {
+class Comms 
+{
     private:
-    static painlessMesh mesh;
-    static QueueHandle_t serialOutPutQueue;
-    static QueueHandle_t meshOutputQueue;
-    static void meshUpdate(void *pvParameters){
-    while(1) 
+
+        static painlessMesh mesh;
+        static QueueHandle_t serialOutPutQueue;
+        static QueueHandle_t meshOutputQueue;
+
+        static void meshUpdate(void *pvParameters)
         {
-            mesh.update();
-            vTaskDelay(30 / portTICK_PERIOD_MS);
+        while(1) 
+            {
+                mesh.update();
+                vTaskDelay(30 / portTICK_PERIOD_MS);
+            }
         }
-    }
-    static void meshBroadCastTask(void *p)
-    {
-        while(1)
+
+        static void meshBroadCastTask(void *p)
         {
-            String msg;
-            if (xQueueReceive(meshOutputQueue, &msg, 10) == pdPASS) 
-            {   
-                if (mesh.sendBroadcast(msg)) 
+            while(1)
+            {
+                char msgChar[256];
+                if (xQueueReceive(meshOutputQueue, &msgChar, 10) == pdPASS) 
+                {   
+                    if (!mesh.sendBroadcast(msgChar)) 
+                    {
+                        Serial.println("Failed to send broadcast");
+                    }
+                }
+                vTaskDelay(50 / portTICK_PERIOD_MS);
+            }
+        }
+
+        static void serialWriteTask(void *p)
+        {
+            while(1)
+            {
+                char msgChar[256];
+                if (xQueueReceive(serialOutPutQueue, &msgChar, 10) == pdPASS) 
+                {   
+                    Serial.println(msgChar); 
+                }
+                vTaskDelay(50 / portTICK_PERIOD_MS);
+            }
+        }
+
+        static void serialReadTask(void *p)
+        {
+            while(1)
+            {
+                //ska läsa strängar från serial och stoppa in i broadcast mesh grajen
+                if(Serial.available()>0)
                 {
-                //    Serial.println("Message sent to all");
-                } 
-                else 
+                    String msg = Serial.readStringUntil('\n');
+                    serialOutPut(msg); // för debug
+                    meshOutPut(msg); // pang på rödbetan
+                }
+                vTaskDelay(30 / portTICK_PERIOD_MS);
+            }
+        }
+
+    public:
+
+        static void meshOutPut(const String& msg)
+        {
+            if (msg != "") 
+            {
+                char msgChar[256];
+                msg.toCharArray(msgChar, sizeof(msgChar));
+                if (!xQueueSend(meshOutputQueue, &msgChar, 10) == pdPASS) 
                 {
-                    Serial.println("Failed to send broadcast");
+                    Serial.println("Failed to add to mesh-queue");
                 }
             }
-            vTaskDelay(50 / portTICK_PERIOD_MS);
         }
-    }
-    static void serialWriteTask(void *p)
-    {
-        while(1)
+            
+        static void serialOutPut(const String& msg)
         {
-            String msg;
-            if (xQueueReceive(serialOutPutQueue, &msg, 10) == pdPASS) 
-            {   
-                Serial.println(msg); 
-            }
-            vTaskDelay(50 / portTICK_PERIOD_MS);
-        }
-    }
-    static void serialReadTask(void *p)
-    {
-        while(1)
-        {
-            //ska läsa strängar från serial och stoppa in i broadcast mesh grajen
-            if(Serial.available()>0)
+            if (msg != "") 
             {
-                String msg = Serial.readStringUntil('\n');
-                serialOutPut(msg); // för debug
-                meshOutPut(msg); // pang på rödbetan
+                char msgChar[256];
+                msg.toCharArray(msgChar, sizeof(msgChar));
+                if (!xQueueSend(serialOutPutQueue, &msgChar, 10) == pdPASS) 
+                {
+                    Serial.println("Failed to add to serial queue");
+                }
+            }
+        }
 
-            }
-            vTaskDelay(30 / portTICK_PERIOD_MS);
-        }
-    }
-    public:
-    static void meshOutPut(const String& msg)
-    {
-        if (msg != "") 
+        Comms() 
         {
-            if (xQueueSend(meshOutputQueue, &msg, 10) == pdPASS) 
-            {
-                //Serial.println(msg + " added to mesh-queue");
-            }
-            else 
-            {
-                Serial.println("Failed to add to mesh-queue");
-            }
+            Serial.begin(115200);
+            Serial.setTimeout(50);
+            mesh.init(MESH_SSID, MESH_PASSWORD, MESH_PORT);
+            mesh.onReceive([](uint32_t from, String &msg) {
+                serialOutPut(msg);
+            });
+            serialOutPutQueue = xQueueCreate(100, sizeof(char)* 256);
+            meshOutputQueue = xQueueCreate(100, sizeof(char)* 256);
+            xTaskCreate(meshUpdate, "meshUpdate", 5000, NULL, 1, NULL);
+            xTaskCreate(serialWriteTask, "serialWriteTask", 5000, NULL, 1, NULL);
+            xTaskCreate(serialReadTask, "serialReadTask", 5000, NULL, 1, NULL);
+            xTaskCreate(meshBroadCastTask, "meshBroadCastTask", 5000, NULL, 1, NULL);
         }
-    }
-        
-    static void serialOutPut(const String& msg)
-    {
-        if (msg != "") 
-        {
-            if (xQueueSend(serialOutPutQueue, &msg, 10) == pdPASS) 
-            {
-                //Serial.println(msg + " added to serial queue");
-                //lyckades lägga till i kön
-            } 
-            else 
-            {
-                Serial.println("Failed to add to serial queue");
-                //buuuu 
-            }
-
-        }
-    }
-    Comms() 
-    {
-        Serial.begin(115200);
-        Serial.setTimeout(50);
-        mesh.init(MESH_SSID, MESH_PASSWORD, MESH_PORT);
-        mesh.onReceive([](uint32_t from, String &msg) {
-            serialOutPut(msg);
-        });
-        serialOutPutQueue = xQueueCreate(10, sizeof(String));
-        meshOutputQueue = xQueueCreate(10, sizeof(String));
-        xTaskCreate(meshUpdate, "meshUpdate", 5000, NULL, 1, NULL);
-        xTaskCreate(serialWriteTask, "serialWriteTask", 5000, NULL, 1, NULL);
-        xTaskCreate(serialReadTask, "serialReadTask", 5000, NULL, 1, NULL);
-        xTaskCreate(meshBroadCastTask, "meshBroadCastTask", 5000, NULL, 1, NULL);
-
-    }
 };
 
 QueueHandle_t Comms::serialOutPutQueue = nullptr;
@@ -122,55 +119,12 @@ QueueHandle_t Comms::meshOutputQueue = nullptr;
 painlessMesh Comms::mesh;
 
 
-void setup() {
-delay(2000);
-Comms comms;
-comms.serialOutPut("Just started");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*  Serial.begin(115200);
-  Serial.setTimeout(50);  // Behövs inte???
-
-  //mesh.setDebugMsgTypes(ERROR | CONNECTION); 
-
-  mesh.init(MESH_SSID, MESH_PASSWORD, MESH_PORT); // Starta meshen
-
-  mesh.onReceive([](uint32_t from, String &msg) {
-    Serial.println(msg.c_str());
-  });
-
-  mesh.onChangedConnections([]() {
-    //printTable();
-  });
-
-    xTaskCreate(meshUpdate, "meshUpdate", 10000, NULL, 1, NULL); // Skapa en task som uppdaterar meshen
-    xTaskCreate(updateFromGUI, "updateFromGUI", 10000, NULL, 1, NULL); // Skapa en task som lyssnar på Serial
-*/    
-
+void setup() 
+{
+    delay(1000);
+    Comms comms;
+    comms.serialOutPut("Just started");
+    comms.meshOutPut("Just started");
 }
 
 void loop()
